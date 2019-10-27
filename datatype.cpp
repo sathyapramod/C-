fc4c62ae7e1b93739a4b9ea6ee27ead5d85b701d
Sathya pramod <sathyapramod12@gmail.com>
2019-10-27 17:08:56 +0530

    Add files via upload

---
+++ datatype.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	cout<<"Size of Char: "<< sizeof(char) << endl;
+	cout<<"Size of int: "<< sizeof(int) << endl;
+	cout<<"Size of float: "<< sizeof(float) <<endl;
+
+	return 0;
+}
+
---
+++ variabletype.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	//variable definition:
+	int a, b;
+	int c;
+	float f;
+	
+	//actual initialization:
+	a = 10;
+	b = 20;
+	c = a + b;
+	cout<< c << endl;
+
+	f = 70.0/3.0;
+	cout<< f << endl;
+
+	return 0;
+}	
